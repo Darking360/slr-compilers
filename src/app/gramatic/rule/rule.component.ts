f2359a82ad20ef05076393ee882e3051a48b18fd
Miguel <MBolivar360@gmail.com>
2017-10-27 22:56:05 -0400

    Boilerplate for gramatic rules

---
+++ src/app/gramatic/rule/rule.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Rule } from './rule';
+
+@Component({
+  selector: 'app-rule',
+  templateUrl: 'gramatic.component.html'
+})
+export class GramaticComponent {
+  name = "Prueba iniciando SLR"
+}
---
+++ src/app/gramatic/rule/rule.ts
@@ -0,0 +1,30 @@
+export class Rule {
+  izq: string;
+  der: string;
+  firstOne: string[];
+  firstOnes: string[];
+  lastOnes: string[];
+
+  constructor(){
+    this.firstOnes = [];
+    this.lastOnes = [];
+    this.firstOne = [];
+  }
+
+  searchEmpty = () => {
+    return this.firstOnes.indexOf('?') != -1;
+  }
+
+  comparewithAnother = (regla: Rule) => {
+    return (this.izq === regla.izq && this.der == regla.der)
+  }
+
+  makeUnique = () => {
+    this.firstOnes = this.firstOnes.filter(function(item, pos, self) {
+      return self.indexOf(item) == pos;
+    });
+    this.lastOnes = this.lastOnes.filter(function(item, pos, self) {
+      return self.indexOf(item) == pos;
+    });
+  }
+}
